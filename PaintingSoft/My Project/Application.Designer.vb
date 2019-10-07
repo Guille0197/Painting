1c1e3d2f0494a7c4f1be6c1a679d35b5bdc09faa
Guillermo Navarro <gnavarro0197@gmail.com>
2019-10-07 12:45:33 -0500

    Error visual studio

--- PaintingSoft/My Project/Application.Designer.vb
+++ PaintingSoft/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PaintingSoft.AboutBox1
+            Me.MainForm = Global.PaintingSoft.Splash
         End Sub
     End Class
 End Namespace
